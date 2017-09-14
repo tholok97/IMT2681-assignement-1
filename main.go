8d6efe13646d12abd93f1e63ed125fbf941b5dc3
tholok97 <thomahl@stud.ntnu.no>
2017-09-14 21:34:26 +0200

    handlers

--- main.go
+++ main.go
@@ -3,18 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/projectinfo/v1/", hello)
+
+	// set up default path
+	http.HandleFunc("/", handleNotFound)
+
+	// set up request handler
+	http.HandleFunc("/projectinfo/v1/", handleRequest)
+
+	// start listening on port 8080
 	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	err := http.ListenAndServe(":8080", nil)
+
+	// if error, panic
 	if err != nil {
 		panic(err)
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, world")
+// handler for when invalid path is used
+func handleNotFound(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "404 - Not found")
+}
+
+// handler for dealing with requests
+func handleRequest(res http.ResponseWriter, req *http.Request) {
+
+	// only GET is legal. only handle GET
+	if req.Method == "GET" {
+
+		// split URL to fetch information ([3:] are interesting)
+		parts := strings.Split(req.URL.String(), "/")
+
+		// print them (FOR DEBUG PURPOSES)
+		for _, p := range parts[3:] {
+			fmt.Fprint(res, p, ", ")
+		}
+		fmt.Fprintln(res)
+
+	} else {
+		// not GET. bad
+		fmt.Fprintln(res, "501 - Not implemented")
+	}
 }
