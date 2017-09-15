3548de85bb338691356b37b89f7b65f5c2b0f6fc
tholok97 <thomahl@stud.ntnu.no>
2017-09-15 12:22:26 +0200

    status codes and payload stub

--- main.go
+++ main.go
@@ -1,22 +1,33 @@
 package main
 
+// TODO: handle erors properly
+
 import (
+	// "encoding/json"
 	"fmt"
 	"net/http"
 	"os"
 	"strings"
 )
 
+type Message struct {
+	Name string
+}
+
 func main() {
 
 	// get the port heroku assignened for us
 	port := os.Getenv("PORT")
 
+	if port == "" { // ....altså hvis heroku ikke har gitt oss en port (DEBUG)
+		port = "8080"
+	}
+
 	// set up default path
-	http.HandleFunc("/", handleNotFound)
+	http.HandleFunc("/", handleBadRequest)
 
 	// set up request handler
-	http.HandleFunc("/projectinfo/v1/", handleRequest)
+	http.HandleFunc("/projectinfo/v1/github.com/", handleRequest)
 
 	// start listening on port 8080
 	fmt.Println("listening on port " + port + "...")
@@ -29,8 +40,9 @@ func main() {
 }
 
 // handler for when invalid path is used
-func handleNotFound(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "404 - Not found")
+func handleBadRequest(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusBadRequest)
+	fmt.Fprintln(res, "400 - Bad request")
 }
 
 // handler for dealing with requests
@@ -42,14 +54,25 @@ func handleRequest(res http.ResponseWriter, req *http.Request) {
 		// split URL to fetch information ([3:] are interesting)
 		parts := strings.Split(req.URL.String(), "/")
 
-		// print them (FOR DEBUG PURPOSES)
-		for _, p := range parts[3:] {
-			fmt.Fprint(res, p, ", ")
+		// hvis den MINST har user (4) og repo (5). hvis ikke, bad request
+		if len(parts) > 5 {
+			user := parts[4]
+			repo := parts[5]
+
+			// generate payload based on user, repo and write to response
+			fmt.Fprintln(res, generateResponsePayload(user, repo))
+		} else {
+			handleBadRequest(res, req)
 		}
-		fmt.Fprintln(res)
 
 	} else {
 		// not GET. bad
+		res.WriteHeader(http.StatusNotImplemented)
 		fmt.Fprintln(res, "501 - Not implemented")
 	}
+}
+
+// send response to client
+func generateResponsePayload(user, repo string) string {
+	return "{\n\t\"project\": \"github.com/" + user + "/" + repo + "\",\n\t\"" + user + "\": \"apache\"\n}"
 }
