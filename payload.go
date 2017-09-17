0eedbb80f7dc278dc006284572b984b7b9232377
tholok97 <thomahl@stud.ntnu.no>
2017-09-17 10:35:23 +0200

    papayload now works

--- payload.go
+++ payload.go
@@ -2,24 +2,49 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-// associated user account/org, indication of prog lang(s) used, account name
-// of top committer (comitter with largest number of commits to the project)
+// Payload contains the information sent back to the user
 type Payload struct {
-	Project   string   `json: "project"`
-	Owner     string   `json: "owner"`
-	Comitter  string   `json "comitter"`
-	Commits   int      `json: "commits"`
-	Languages []string `json: "languages"`
+	Project   string   `json:"project"`
+	Owner     string   `json:"owner"`
+	Comitter  string   `json:"comitter"`
+	Commits   int      `json:"commits"`
+	Languages []string `json:"languages"`
 }
 
 // Contains information we wanted from the initial github request
 type githubReposResponse struct {
-	Contributors_url string `json: "contributors_url"` // url to contributors
-	Languages_url    string `json: "languages_url"`    // url to langauges
+	ContributorsURL string `json:"contributors_url"` // url to contributors
+	LanguagesURL    string `json:"languages_url"`    // url to langauges
+}
+
+// contains info we gain from the "/contributors" request on github
+type contributor struct {
+	Login         string `json:"login"`
+	Contributions int    `json:"contributionsi"`
+}
+
+// returns top contributor in slice of contributors. nil if empty
+func topContributor(contrs []contributor) contributor {
+
+	// empty slice: give up
+	if len(contrs) < 1 {
+		return contributor{}
+	}
+
+	// find top contributor
+	top := contrs[0]
+	for _, value := range contrs {
+		if value.Contributions > top.Contributions {
+			top = value
+		}
+	}
+
+	return top
 }
 
 // generate payload by requesting github for the info we need and then basing
@@ -27,7 +52,7 @@ type githubReposResponse struct {
 func generateResponsePayload(user, repo string) (Payload, error) {
 
 	// (try to) get json from url pointing to $user, $repo
-	respJson, getErr := getJson("https://api.github.com/repos/" + user +
+	respJSON, getErr := getJSON("https://api.github.com/repos/" + user +
 		"/" + repo)
 	if getErr != nil {
 		return Payload{}, getErr
@@ -35,7 +60,7 @@ func generateResponsePayload(user, repo string) (Payload, error) {
 
 	// (try to) fill ghReposResp with data from json response
 	var ghReposResp githubReposResponse
-	jsonErr := json.Unmarshal(respJson, &ghReposResp)
+	jsonErr := json.Unmarshal(respJSON, &ghReposResp)
 	if jsonErr != nil {
 		return Payload{}, jsonErr
 	}
@@ -44,15 +69,14 @@ func generateResponsePayload(user, repo string) (Payload, error) {
 	var pload = Payload{Owner: user, Project: repo}
 
 	// (try to) determine top commiter. if unable, leave blank
-	topCommiter, commitNum, commitErr :=
-		determineTopCommiter(ghReposResp.Contributors_url)
+	top, commitErr := determineTopCommiter(ghReposResp.ContributorsURL)
 	if commitErr == nil {
-		pload.Comitter = topCommiter
-		pload.Commits = commitNum
+		pload.Comitter = top.Login
+		pload.Commits = top.Contributions
 	}
 
 	// (try to) determine langauges. if unable, leave blank
-	langauges, langErr := determineLanguages(ghReposResp.Languages_url)
+	langauges, langErr := determineLanguages(ghReposResp.LanguagesURL)
 	if langErr == nil {
 		pload.Languages = langauges
 	}
@@ -61,8 +85,28 @@ func generateResponsePayload(user, repo string) (Payload, error) {
 }
 
 // based on url from github payload ^, get top commiters name and num of commits
-func determineTopCommiter(url string) (string, int, error) {
-	return "julenissen", 923, nil
+func determineTopCommiter(url string) (contributor, error) {
+
+	// (try to) get json ffrom url as []byte
+	respJSON, getErr := getJSON(url)
+	if getErr != nil {
+		return contributor{}, getErr
+	}
+
+	// (try to) parse json into list of contributors
+	contrs := make([]contributor, 0)
+	marshalErr := json.Unmarshal(respJSON, &contrs)
+	if marshalErr != nil {
+		return contributor{}, marshalErr
+	}
+
+	// find top contributor. (if contrs is empty, give up...)
+	top := topContributor(contrs)
+	if top.Login == "" {
+		return contributor{}, fmt.Errorf("Empty contributors slice. Something is wrong")
+	}
+
+	return top, nil
 }
 
 // try to get the languages used in the project based on url pointing to json
@@ -70,14 +114,14 @@ func determineTopCommiter(url string) (string, int, error) {
 func determineLanguages(url string) ([]string, error) {
 
 	// (try to) get json from url as []byte
-	respJson, getErr := getJson(url)
+	respJSON, getErr := getJSON(url)
 	if getErr != nil {
 		return nil, getErr
 	}
 
 	// (try to) unmarshal json info into map of { language: bytes }
 	var data map[string]interface{}
-	marshalErr := json.Unmarshal([]byte(respJson), &data)
+	marshalErr := json.Unmarshal([]byte(respJSON), &data)
 	if marshalErr != nil {
 		return nil, marshalErr
 	}
@@ -93,11 +137,11 @@ func determineLanguages(url string) ([]string, error) {
 
 // get json that url points to and return it as []byte. return error if
 // anything goes wrong
-func getJson(url string) ([]byte, error) {
+func getJSON(url string) ([]byte, error) {
 
 	// (try to) get response from url
 	resp, getErr := http.Get(url)
-	if getErr != nil {
+	if getErr != nil || resp.StatusCode != http.StatusOK {
 		return nil, getErr
 	}
 
